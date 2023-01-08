d37ec2a8a22c0254fa8b937570f9fa0f33e02213
ElizavetaShangina <118834113+ElizavetaShangina@users.noreply.github.com>
2023-01-08 13:31:41 +0300

    Добавлена фоновая музыка

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
+import pygame.time
+
 from first_window import draw_background, draw_first_window
 from player_names import input_player_names
 from character_selecting_menu import draw_menu
 
+
+intro_sound = pygame.mixer.Sound('project_files\\intro.wav')
+pygame.mixer.Sound.play(intro_sound)
+
 draw_first_window()
+
+font = pygame.mixer.Sound('project_files\\select.wav')
+pygame.mixer.Sound.play(font, -1)
+
 player1, player2 = input_player_names()
 player1_character, player2_character = draw_menu(player1, player2)
 
-# Подключение самой игры
+pygame.mixer.Sound.stop(font)
+print(player1, player2)
